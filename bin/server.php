e4c2dcbb0847a98fccab2612da949184283bef28
Pawan Kumar <surpawan@gmail.com>
2016-09-04 18:08:15 +0530

    fixed server start issue

--- bin/server.php
+++ bin/server.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Chat\Chat;
 
 use Ratchet\Server\IoServer;
-use Ratchet\http\HttpServer;
+use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
 $server = IoServer::factory(new HttpServer(new WsServer(new Chat)), 8080);
