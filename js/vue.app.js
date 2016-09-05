33965befedec88d0a8361e086a3650adc4449419
Pawan Kumar <pawankumar@Pawans-Mac-mini.local>
2016-09-05 19:07:38 +0530

    Added Users list

--- js/vue.app.js
+++ js/vue.app.js
@@ -1,7 +1,7 @@
 new Vue({
     el: "#chatApp",
     data: {
-        usercount: "",
+        usercount: null,
         username: "",
         conn: null,
         chatwindow: "",
@@ -42,12 +42,12 @@ new Vue({
         addChatMessage: function (name, msg) {
 
             this.chats.push({type: 'user', message: msg, name: name, time: new Date(Date.now()).toLocaleTimeString()});
-            //this.chatwindow.innerHTML += '<p id="chat-user-msg"><span>' + name + '> </span> ' + msg + '(' + new Date(Date.now()).toLocaleTimeString() + ')' + '</p>';
+            document.getElementById("chatwindow").scrollTop = document.getElementById("chatwindow").scrollHeight + 40;
         },
 
         addSystemMessage: function (msg) {
             this.chats.push({type: 'system', message: msg, name: 'system', time: new Date(Date.now()).toLocaleTimeString()});
-            //this.chatwindow.innerHTML += '<p id="chat-system-msg">' + msg + '(' + new Date(Date.now()).toLocaleTimeString() + ')' + '</p>';
+            document.getElementById("chatwindow").scrollTop = document.getElementById("chatwindow").scrollHeight + 40;
         },
         setupConnectionEvents: function () {
             var self = this;
@@ -75,20 +75,23 @@ new Vue({
             var data = JSON.parse(evt.data);
             switch (data.action) {
                 case 'setname':
-                    if (data.success) {
-                        this.addSystemMessage("Set username to " + this.username);
-                    } else {
-                        this.addSystemMessage("Username " + this.username + " has been taken.");
-                    }
-                    break;
+                if (data.success) {
+                    this.addSystemMessage("Set username to " + this.username);
+                } else {
+                    this.addSystemMessage("Username " + this.username + " has been taken.");
+                }
+                break;
                 case 'message':
-                    this.addChatMessage(data.username, data.msg);
-                    break;
+                this.addChatMessage(data.username, data.msg);
+                break;
                 case 'usercount':
-                    this.usercount = data.usercount + (data.usercount === "1" ? ' user online' : ' users online');
-                    break;
+                this.usercount = data.usercount;
+                // console.log(data.newuser);
+                console.log(data.newuser);
+                this.users = data.newuser;
+                break;
                 default:
-                    break;
+                break;
 
             }
         },
